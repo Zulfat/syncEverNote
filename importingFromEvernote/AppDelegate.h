f2c6b016ce2eece463ef2e9da59251b3cb0a3b86
Zulfat <zulfat@138.1.168.192.in-addr.arpa>
2014-01-14 11:07:59 +0400

    Initial Commit

---
+++ importingFromEvernote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  importingFromEvernote
+//
+//  Created by Zulfat on 14.01.14.
+//  Copyright (c) 2014 Zulfat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ importingFromEvernote/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  importingFromEvernote
+//
+//  Created by Zulfat on 14.01.14.
+//  Copyright (c) 2014 Zulfat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
